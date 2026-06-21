26759d8819d19fd5eb12232953b0b9a26c960e5b
hemantdidel <hemantdidel2002@gmail.com>
2026-06-21 13:20:17 +0530

    Update review.js

--- js/review.js
+++ js/review.js
@@ -1,1 +1,120 @@
+"use strict";
 
+const result = JSON.parse(
+
+localStorage.getItem("quiz_result")
+
+);
+
+if(!result){
+
+window.location.href="index.html";
+
+}
+
+const container = document.getElementById(
+
+"reviewContainer"
+
+);
+
+result.questions.forEach((question,index)=>{
+
+const card=document.createElement("div");
+
+card.className="reviewCard";
+
+const user=result.answers[index];
+
+const correct=question.answer;
+
+let status="Skipped";
+let statusClass="skipStatus";
+
+if(user!==null){
+
+if(user===correct){
+
+status="Correct";
+statusClass="correctStatus";
+
+}else{
+
+status="Wrong";
+statusClass="wrongStatus";
+
+}
+
+}
+
+let html=`
+
+<span class="status ${statusClass}">
+
+${status}
+
+</span>
+
+<h3>
+
+Q${index+1}. ${question.question}
+
+</h3>
+
+`;
+
+question.options.forEach((option,i)=>{
+
+let cls="option";
+
+if(i===correct){
+
+cls+=" correct";
+
+}
+
+if(
+
+user===i &&
+
+user!==correct
+
+){
+
+cls+=" wrong";
+
+}
+
+html+=`
+
+<div class="${cls}">
+
+${option}
+
+</div>
+
+`;
+
+});
+
+if(question.explanation){
+
+html+=`
+
+<p>
+
+<b>Explanation:</b>
+
+${question.explanation}
+
+</p>
+
+`;
+
+}
+
+card.innerHTML=html;
+
+container.appendChild(card);
+
+});
