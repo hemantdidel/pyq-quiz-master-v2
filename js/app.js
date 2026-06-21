76c4b2d289f9acb1db4ba4283e680d387f4209f8
hemantdidel <hemantdidel2002@gmail.com>
2026-06-21 09:55:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,57 @@
+// Load Categories
 
+fetch("data/categories.json")
+
+.then(response=>response.json())
+
+.then(data=>{
+
+let container=document.getElementById("categoryContainer");
+
+let html="";
+
+data.forEach(category=>{
+
+html+=`
+
+<div class="card">
+
+<h2>
+
+${category.name}
+
+</h2>
+
+<p>
+
+Click to explore practice tests
+
+</p>
+
+<a
+
+class="btn"
+
+href="${category.page}">
+
+Open
+
+</a>
+
+</div>
+
+`;
+
+});
+
+container.innerHTML=html;
+
+})
+
+.catch(()=>{
+
+document.getElementById("categoryContainer").innerHTML=
+
+"<h2>Unable to load categories.</h2>";
+
+});
